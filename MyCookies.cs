f3bb66c58870139d42cfe941f952ef3e22cf087d
wpqs <wpq.stott@gmail.com>
2020-07-30 16:49:09 +0200

    Add project files.

---
+++ MyCookies.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace BugCookieNotSet
+{
+    public class MyCookies
+    {
+        public IHttpContextAccessor Accessor { get; private set; }
+
+        public MyCookies(IHttpContextAccessor accessor)
+        {
+            Accessor = accessor;
+        }
+
+        public string Get(string name)
+        {
+            var rc = "fail";
+            if (Accessor.HttpContext.Request.Cookies.TryGetValue(name, out var result)) 
+                rc = $"{name}={result}"; 
+            return rc;
+        }
+
+        public bool Set(string name, string value)
+        {
+            var rc = false;
+            Accessor.HttpContext.Response.Cookies.Append(name, value);
+            if (Accessor.HttpContext.Request.Cookies.TryGetValue(name, out var result) && (result == value))
+                rc = true;
+            return rc;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BugCookieNotSet.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        [BindProperty]
+        public string Comment { get; set; }
+        public IndexModel(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void OnGet()
+        {
+            var result = true;
+            var cookies = new MyCookies(_httpContextAccessor);
+            if (cookies.Set(".AspNetCore.Culture", "fr-CH") == false)
+                result = false;
+            Comment = (result) ? $"Success: {cookies.Get(".AspNetCore.Culture")}" : $"{cookies.Get(".AspNetCore.Culture")}";
+
+        }
+    }
+}
